279809f1ca688a33013dd258d1444bf3662da514
deliciousmonster <jaxon@deliciousmonster.com>
2019-04-11 11:37:30 -0600

    clean up

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,11 +7,7 @@ printf "\n[-] Installing base OS dependencies...\n\n"
 # install base dependencies
 
 apt-get update
-
-# ensure we can get an https apt source if redirected
-# https://github.com/jshimko/meteor-launchpad/issues/50
 apt-get install -y apt-transport-https ca-certificates
-
 apt-get install -y --no-install-recommends curl bzip2 bsdtar build-essential python git wget gnupg2 dirmngr
 
 dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
